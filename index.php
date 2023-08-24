eeec670d032b7972eb53c9a718ba8ba5db284e4a
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-08-24 08:38:06 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD PHP e SQL</title>
+</head>
+<body>
+    <h1>Sistema Vendas - CRUD PHP e SQL</h1>
+    <hr>
+    <p>Operações de <b>inserção</b>, <b>leitura</b>, <b>atualização</b> e <b>exclusão</b> de dados.</p>
+    <hr>
+    <h2>Escolha o que deseja gerenciar:</h2>
+
+    <ul>
+        <li><a href="">Fabricantes</a></li>
+        <li><a href="">Produtos</a></li>
+    </ul>
+
+</body>
+</html>
