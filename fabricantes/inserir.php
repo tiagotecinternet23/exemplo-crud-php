5e00676591e129e5ebb3b7d21df74585bd341343
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-08-24 08:55:22 -0300

    Página de inserção de fabricante

---
+++ fabricantes/inserir.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fabricantes - Inserção</title>
+</head>
+<body>
+    <h1>Fabricantes | INSERT</h1>
+    <hr>
+    <form action="" method="post">
+        <p>
+            <label for="nome">Nome:</label>
+            <input type="text" name="nome" id="nome">
+        </p>
+        <button type="submit" name="inserir">Inserir fabricante</button>
+    </form>
+</body>
+</html>
--- fabricantes/visualizar.php
+++ fabricantes/visualizar.php
@@ -9,9 +9,12 @@
     <h1>Fabricantes | SELECT - 
         <a href="../index.php">Home</a>
     </h1>
-    
+
     <hr>
     <h2>Lendo e carregando todos os fabricantes.</h2>
+
+    <p><a href="inserir.php">
+        Inserir novo fabricante</a></p>
 
 </body>
 </html>
