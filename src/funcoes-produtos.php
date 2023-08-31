6daf4a187f85cba4c7c07fceba339680d17867e8
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-08-31 09:39:39 -0300

    Update funcoes-produtos.php

--- src/funcoes-produtos.php
+++ src/funcoes-produtos.php
@@ -76,14 +76,24 @@ function lerUmProduto(PDO $conexao, int $id):array {
 }
 
 
-function atualizarProduto(
-    PDO $conexao, 
-    int $id, 
-    string $nome, 
-    float $preco, 
-    int $quantidade, 
-    string $descricao, 
-    int $fabricanteId
-):void {
-    
+function atualizarProduto(PDO $conexao, int $id, string $nome, float $preco, int $quantidade, string $descricao, int $fabricanteId):void {
+
+    $sql = "UPDATE produtos SET
+        nome = :nome,
+        preco = :preco,
+        quantidade = :quantidade,
+        descricao = :descricao,
+        fabricante_id = :fabricanteId WHERE id = :id";
+    try {
+        $consulta = $conexao->prepare($sql);
+        $consulta->bindValue(":nome", $nome, PDO::PARAM_STR);
+        $consulta->bindValue(":preco", $preco, PDO::PARAM_STR);
+        $consulta->bindValue(":quantidade", $quantidade, PDO::PARAM_INT);
+        $consulta->bindValue(":descricao", $descricao, PDO::PARAM_STR);
+        $consulta->bindValue(":fabricanteId", $fabricanteId, PDO::PARAM_INT);
+        $consulta->bindValue(":id", $id, PDO::PARAM_INT);
+        $consulta->execute();
+    } catch (Exception $erro) {
+        die("Erro ao atualizar: ".$erro->getMessage());
+    }   
 }
