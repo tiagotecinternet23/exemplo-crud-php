c388b62509cdea63bd848cc9225cb1dbfc024da9
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-08-31 09:53:29 -0300

    Função de delete de produto

--- src/funcoes-produtos.php
+++ src/funcoes-produtos.php
@@ -97,3 +97,24 @@ function atualizarProduto(PDO $conexao, int $id, string $nome, float $preco, int
         die("Erro ao atualizar: ".$erro->getMessage());
     }   
 }
+
+
+function excluirProduto(PDO $conexao, int $id):void {
+    $sql = "DELETE FROM produtos WHERE id = :id";
+    try {
+        $consulta = $conexao->prepare($sql);
+        $consulta->bindValue(":id", $id, PDO::PARAM_INT);
+        $consulta->execute();
+    } catch (Exception $erro) {
+        die("Erro ao excluir: ".$erro->getMessage());
+    }
+}
+
+
+
+
+
+
+
+
+
